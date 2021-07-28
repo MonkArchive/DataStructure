2e01f1703ddff2744b599f3bd7d54c20317c27d9
Sandeep Sharma <sandeepksharma15@gmail.com>
2021-07-28 10:45:14 +0530

    . Restructured Solution

---
+++ DataStructure/Stack/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
