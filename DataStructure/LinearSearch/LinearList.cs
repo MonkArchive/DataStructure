523ff19f64b75311f4203a871c5dea9d37321ec7
Sandeep Sharma <sandeepksharma15@gmail.com>
2021-08-04 12:12:53 +0530

    . Lineasr Search

---
+++ DataStructure/LinearSearch/LinearList.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Drawing;
+
+namespace LinearSearch
+{
+    public class LinearList : ILinearList
+    {
+        int[] list;
+        int size;
+
+        public LinearList( int size )
+        {
+            this.size = size;
+            list = new int[size];
+        }
+
+        public bool Contains( int item )
+        {
+            for (int i = 0; i < size; i++)
+                if (list[i] == item)
+                    return true;
+
+            return false;
+        }
+
+        public void DisplayData()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write( list[i] + " " );
+            }
+
+            Console.WriteLine();
+        }
+
+        public void FillWithRandomData( int start = 1, int end = 100 )
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < size; i++)
+            {
+                list[i] = random.Next( start, end );
+            }
+        }
+
+        public int FindFirst( int item )
+        {
+            for (int i = 0; i < size; i++)
+                if (list[i] == item)
+                    return i;
+
+            return -1;
+        }
+
+        public int FindOccurences( int item )
+        {
+            int count = 0;
+
+            for (int i = 0; i < size; i++)
+                if (list[i] == item)
+                    ++count;
+
+            return count;
+        }
+
+        public void GetUserData()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write( $"List[{i}]: " );
+                string strInput = Console.ReadLine();
+                list[i] = Convert.ToInt32( strInput );
+            }
+        }
+    }
+
+    public interface ILinearList
+    {
+        void GetUserData();
+        void FillWithRandomData( int start = 1, int end = 100 );
+        void DisplayData();
+        int FindFirst( int item );          // Return Index Of That Item, -1 if absent
+        int FindOccurences( int item );     // Return The Number Of Times Item Is Present In The List
+        bool Contains( int item );          // Return True / False Depending Upon If The Item Exists
+    }
+}
---
+++ DataStructure/LinearSearch/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LinearSearch
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            ILinearList list = new LinearList(10);
+
+            list.FillWithRandomData();
+            list.DisplayData();
+
+            Console.Write( "Enter The Number To Search: " );
+            string strInput = Console.ReadLine();
+            int number = Convert.ToInt32(strInput);
+
+            Console.WriteLine( $"{number} Exists In The List At {list.FindFirst( number )} Index" );
+            Console.WriteLine( $"{number} Exists In The List {list.FindOccurences( number )} Times" );
+            Console.WriteLine( $"{number} Present In The List: {list.Contains( number )}" );
+        }
+    }
+}
