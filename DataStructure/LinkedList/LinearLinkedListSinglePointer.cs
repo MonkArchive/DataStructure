9a5522cec3f3c712cf398ffc8a97de55d8041a28
Sandeep Sharma <sandeepksharma15@gmail.com>
2021-08-17 13:13:28 +0530

    . Code Reorganization and Renaming

--- DataStructure/LinkedList/LinearLinkedListSinglePointer.cs
+++ DataStructure/LinkedList/LinearLinkedListSinglePointer.cs
@@ -408,32 +408,6 @@ namespace LinkedList
         }
 
         #endregion
-
-    }
-
-    public interface ILinkedList<T>
-    {
-        void Append( INode<T> node );
-        INode<T> Search( T element );
-        INode<T> Remove( INode<T> node );
-        void Clear();
-        long Count();
-        bool IsEmpty();
-        void InsertAfter( INode<T> node, INode<T> newNode );
-        void InsertBefore( INode<T> node, INode<T> newNode );
-        void BubbleSort();
-        void InsertionSort();
-        void SelectionSort();
-        void MergeSort();
-        void RemoveDuplicates();
-        void Reverse();
-        bool ContainsCycles();
-        void Display();
-        void Prepend( INode<T> node );
-        bool IsSorted( bool DescendingOrder = false );
-        void InsertSorted( INode<T> newNode );      // Assumption: List Is In Ascending Order
-        INode<T> SplitAlternateNode();
-        INode<T> SplitInTheMiddle();
     }
 }
 
