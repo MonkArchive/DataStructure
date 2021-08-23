5db78634949f99388f466c9856b0ed970edc97f8
Sandeep Sharma <sandeepksharma15@gmail.com>
2021-08-23 13:01:53 +0530

    . Circular Linked List

---
+++ DataStructure/CircularLinkedList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CircularLinkedList
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            ILinkedList<int> list = new CircularList<int>();
+
+            list.Append( new Node<int>( 10 ) );
+            list.Append( new Node<int>( 20 ) );
+            list.Append( new Node<int>( 30 ) );
+            list.Prepend( new Node<int>( 40 ) );
+
+            list.Remove( list.Search( 10 ) );
+            list.Remove( list.Search( 30 ) );
+            list.Remove( list.Search( 40 ) );
+            list.Remove( list.Search( 20 ) );
+        }
+    }
+}
