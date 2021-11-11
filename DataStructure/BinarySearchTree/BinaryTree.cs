e0ad048476fb8946f20996d00bc7e39e4befcbe0
Sandeep Sharma <sandeepksharma15@gmail.com>
2021-11-11 11:54:20 +0530

    . BST

--- DataStructure/BinarySearchTree/BinaryTree.cs
+++ DataStructure/BinarySearchTree/BinaryTree.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 namespace BinarySearchTree
 {
@@ -27,9 +28,87 @@ namespace BinarySearchTree
             PostOrderTraversal(_root, ProcessNode);
         }
 
+        public void LevelByLevelTraversal(Action<TreeNode> ProcessNode)
+        {
+            CircularCountQueue<TreeNode> queue = new CircularCountQueue<TreeNode>(20);
+
+            queue.Add(_root);
+
+            while (!queue.IsEmpty())
+            {
+                TreeNode p = queue.Remove();
+
+                ProcessNode(p);
+
+                if (p.Left is not null)
+                    queue.Add(p.Left);
+
+                if (p.Right is not null)
+                    queue.Add(p.Right);
+            }
+        }
+
         public void InOrder(Action<TreeNode> ProcessNode)
         {
             InOrderTraversal(_root, ProcessNode);
+        }
+
+        public TreeNode Search(int data)
+        {
+            return SearchTree(_root, data);
+        }
+
+        public int CountLeaves()
+        {
+            return CountLeaves(_root);
+        }
+
+        public int CountNodes()
+        {
+            return CountNodes(_root);
+        }
+
+        public int Height()
+        {
+            return Height(_root);
+        }
+
+        private static int Height(TreeNode root)
+        {
+            if (root is null)
+                return 0;
+            else
+                return Math.Max(Height(root.Left), Height(root.Right)) + 1;
+        }
+
+        private static int CountNodes(TreeNode root)
+        {
+            if (root is null)
+                return 0;
+            else
+                return CountNodes(root.Left) + CountNodes(root.Right) + 1;
+        }
+
+        private static int CountLeaves(TreeNode root)
+        {
+            if (root is null)
+                return 0;
+            else if (root.Left is null && root.Right is null)
+                return 1;
+            else
+                return CountLeaves(root.Left) + CountLeaves(root.Right);
+        }
+
+        private static TreeNode SearchTree(TreeNode root, int data)
+        {
+            if (root is null)
+                return null;
+            else if (root.Data == data)
+                return root;
+            else if (root.Data > data)
+                return SearchTree(root.Left, data);
+            else
+                return SearchTree(root.Right, data);
         }
 
         private static TreeNode InsertNode(TreeNode root, TreeNode node)
--- DataStructure/BinarySearchTree/Program.cs
+++ DataStructure/BinarySearchTree/Program.cs
@@ -17,6 +17,16 @@ namespace BinarySearchTree
             binaryTree.InOrder(PrintNodeData);
             binaryTree.PostOrder(PrintNodeData);
 
+            binaryTree.LevelByLevelTraversal(PrintNodeData);
+
+            Console.WriteLine();
+
+            Console.WriteLine($"3 Exists in the database: {binaryTree.Search(3) is not null}");
+            Console.WriteLine($"13 Exists in the database: {binaryTree.Search(13) is not null}");
+
+            Console.WriteLine($"This Tree Has {binaryTree.CountLeaves()} Leaves");
+            Console.WriteLine($"This Tree Has {binaryTree.CountNodes()} Nodes");
+            Console.WriteLine($"This Tree Has Height of {binaryTree.Height()}");
         }
 
         public static void PrintNodeData(TreeNode node)
