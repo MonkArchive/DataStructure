50ff97e909c435b8ac8fd773982263e02cccc417
Sandeep Sharma <sandeepksharma15@gmail.com>
2021-11-24 12:12:56 +0530

    . Weighted Graphs

--- DataStructure/Graphs/WeightedGraph.cs
+++ DataStructure/Graphs/WeightedGraph.cs
@@ -6,17 +6,25 @@
 
         public virtual void AddEdge(string x, string y, int w)
         {
-            throw new System.NotImplementedException();
+            SetEdgeWeight(x, y, w);
         }
 
         public virtual int GetEdgeWeight(string x, string y)
         {
-            throw new System.NotImplementedException();
+            var indexX = GetVertexIndex(x);
+            var indexY = GetVertexIndex(y);
+
+            return _adMatrix[indexX, indexY];
         }
 
         public virtual void SetEdgeWeight(string x, string y, int w)
         {
-            throw new System.NotImplementedException();
+            var indexX = GetVertexIndex(x);
+            var indexY = GetVertexIndex(y);
+
+            // 1 Means Edge Exists; MinInt Means Edge Doesn't Exist
+            _adMatrix[indexX, indexY] = w;
+            _adMatrix[indexY, indexX] = w; // Skip This In Directed Graph
         }
 
         public virtual int ShortestWeightPath(string x, string y)
