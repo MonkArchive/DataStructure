9bf57f6e3800ae0d0f064bafdbdd3166dd072f90
Sandeep Sharma <sandeepksharma@hotmail.com>
2021-12-15 11:50:22 +0530

    . Dynamic

---
+++ DataStructure/Dynamic/Alogorithms.cs
@@ -0,0 +1,28 @@
+﻿namespace Dynamic;
+
+public class Alogorithms
+{
+    static int[] Fibo = new int[100]; 
+
+    /// <summary>
+    /// Return The Nth Fibonnaci Number
+    /// </summary>
+    /// <param name="n">Max Limit: 100</param>
+    /// <returns>Nth Fibo</returns>
+    public static int NthFibonacci(int n)
+    {
+        int f;
+
+        if (Fibo[n] != 0)
+            return Fibo[n];
+
+        if ((n == 1) || (n == 2))
+            f = 1;
+        else
+            f = NthFibonacci(n - 1) + NthFibonacci(n - 2);
+
+        Fibo[n] = f;
+
+        return f;
+    }
+}
---
+++ DataStructure/Dynamic/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Dynamic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"10th Fibonacci Number Is {Alogorithms.NthFibonacci(10)}");
+    }
+}
